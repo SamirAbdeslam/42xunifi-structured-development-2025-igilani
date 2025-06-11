ca5a2c97dd7a97772bd5285e1052e9c432aa77ea
Samir Abdeslam <ilijuve@hotmail.it>
2025-06-11 16:47:55 +0200

    Implement average calculation function and header file

---
+++ ex00/average.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   average.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igilani <igilani@student.42firenze.it>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 13:45:46 by igilani           #+#    #+#             */
+/*   Updated: 2025/06/10 14:04:22 by igilani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "average.h"
+
+float average(const int *arr, int size)
+{
+	int i = 0;
+	int sum = 0;
+	int div = size;
+
+	while (i < size)
+	{
+		printf("%d\n", arr[i]);
+		if (arr[i] >= 0 && arr[i] <= 100)
+		{
+			sum += arr[i];
+			i++;
+		}
+		else
+		{
+			div--;
+			i++;
+		}
+	}
+	return ((float)sum / div);
+}
+
+// int main()
+// {
+// 	const int *arr = (int[]){1, 2, 3, 4, 200};
+// 	int size = 5;
+// 	float avg = average(arr, size);
+// 	printf("Average: %.2f\n", avg);
+// 	return 0;
+// }
---
+++ ex00/average.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   average.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igilani <igilani@student.42firenze.it>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 13:46:02 by igilani           #+#    #+#             */
+/*   Updated: 2025/06/10 13:47:57 by igilani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AVERAGE_H
+# define AVERAGE_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+float average(const int *arr, int size);
+
+#endif
